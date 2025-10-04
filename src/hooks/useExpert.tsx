caae6027690ae8d4538efa4ff6a8cce4e80b2743
Chibuike Onwugharam <michaelfrancis135@gmail.com>
2025-10-04 15:36:33 +0100

    Messages integrated

--- src/hooks/useExpert.tsx
+++ src/hooks/useExpert.tsx
@@ -4,55 +4,111 @@ import { AxiosError } from "axios";
 import { toast } from "sonner";
 
 export const useGetExperts = (params?: any) => {
-    const { data, isLoading } = useQuery({
-        queryKey: ["experts", params],
-        queryFn: async () => {
-            try {
-                const response = await axiosClient.get(`/experts/business?${new URLSearchParams(params).toString()}`)
-                if (response.data?.status === false) {
-                    throw new Error(response.data?.message || "Failed to fetch experts");
-                }
-                return response.data?.data
-            } catch (error: any) {
-                if (error instanceof AxiosError) {
-                    toast.error(error.response?.data?.message || "Failed to fetch experts")
-                } else if (error instanceof Error) {
-                    toast.error(error.message)
-                } else {
-                    toast.error("An unexpected error occured while fetching experts")
-                }
+	const { data, isLoading } = useQuery({
+		queryKey: ["experts", params],
+		queryFn: async () => {
+			try {
+				const response = await axiosClient.get(
+					`/experts/business?${new URLSearchParams(
+						params
+					).toString()}`
+				);
+				if (response.data?.status === false) {
+					throw new Error(
+						response.data?.message || "Failed to fetch experts"
+					);
+				}
+				return response.data?.data;
+			} catch (error: any) {
+				if (error instanceof AxiosError) {
+					toast.error(
+						error.response?.data?.message ||
+							"Failed to fetch experts"
+					);
+				} else if (error instanceof Error) {
+					toast.error(error.message);
+				} else {
+					toast.error(
+						"An unexpected error occured while fetching experts"
+					);
+				}
 
-                throw error;
-            }
-        }
-    })
+				throw error;
+			}
+		},
+	});
 
-    return { experts: data, isLoading }
-}  
+	return { experts: data, isLoading };
+};
 
 export const useGetExpert = (expertId: string) => {
-    const { data, isLoading } = useQuery({
-        queryKey: ["experts", expertId],
-        queryFn: async () => {
-            try {
-                const response = await axiosClient.get(`/expert/${expertId}/business`)
-                if (response.data?.status === false) {
-                    throw new Error(response.data?.message || "Failed to fetch expert");
-                }
-                return response.data?.data
-            } catch (error: any) {
-                if (error instanceof AxiosError) {
-                    toast.error(error.response?.data?.message || "Failed to fetch expert")
-                } else if (error instanceof Error) {
-                    toast.error(error.message)
-                } else {
-                    toast.error("An unexpected error occured while fetching expert")
-                }
+	const { data, isLoading } = useQuery({
+		queryKey: ["experts", expertId],
+		queryFn: async () => {
+			try {
+				const response = await axiosClient.get(
+					`/expert/${expertId}/business`
+				);
+				if (response.data?.status === false) {
+					throw new Error(
+						response.data?.message || "Failed to fetch expert"
+					);
+				}
+				return response.data?.data;
+			} catch (error: any) {
+				if (error instanceof AxiosError) {
+					toast.error(
+						error.response?.data?.message ||
+							"Failed to fetch expert"
+					);
+				} else if (error instanceof Error) {
+					toast.error(error.message);
+				} else {
+					toast.error(
+						"An unexpected error occured while fetching expert"
+					);
+				}
 
-                throw error;
-            }
-        }
-    })
+				throw error;
+			}
+		},
+	});
 
-    return { expert: data, isLoading }
-} 
+	return { expert: data, isLoading };
+};
+
+export const useGetAdmins = () => {
+	const { data, isLoading } = useQuery({
+		queryKey: ["Admins"],
+		queryFn: async () => {
+			try {
+				const response = await axiosClient.get(
+					`/admin-business?roles=super,support,vetting`
+				);
+				if (response.data?.status === false) {
+					throw new Error(
+						response.data?.message || "Failed to fetch expert"
+					);
+				}
+				return response.data?.data;
+			} catch (error: any) {
+				if (error instanceof AxiosError) {
+					toast.error(
+						error.response?.data?.message ||
+							"Failed to fetch expert"
+					);
+				} else if (error instanceof Error) {
+					toast.error(error.message);
+				} else {
+					toast.error(
+						"An unexpected error occured while fetching expert"
+					);
+				}
+
+				throw error;
+			}
+		},
+	});
+
+	return { admins: data, isLoading };
+};
